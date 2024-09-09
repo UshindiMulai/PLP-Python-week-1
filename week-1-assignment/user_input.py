4f6279b63f9d77fba0f1523ba30e2f3a32c8ec0c
UshindiMulai <ushindimulai@gmail.com>
2024-09-09 15:51:05 +0300

    Python week-1: User input form program

---
+++ week-1-assignment/user_input.py
@@ -0,0 +1,11 @@
+# Get the user's name
+name = input("What is your name? ")
+
+# Get the user's age
+age = input("How old are you? ")
+
+# Get the user's location
+location = input("Where do you live? ")
+
+# Print out a personalized message
+print("Hello", name, ", you are", age, "years old and live in", location)
